f57d5caf1b7e07ddba4cc4db65795bfffc904125
pierre <pierre.bultez@proton.me>
2025-11-20 18:40:32 +0100

    Refactor WooCommerce cart templates to improve layout consistency and readability: remove redundant wrappers, adjust text alignment, refine styling, and extend `.gitignore` to exclude additional screenshots.

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -219,15 +219,17 @@ do_action( 'woocommerce_before_cart' ); ?>
 
     <div class="lg:col-span-1">
         <?php do_action( 'woocommerce_before_cart_collaterals' ); ?>
-        <div class="cart-collaterals">
-            <?php
-            /**
-             * Cart collaterals hook.
-             * @hooked woocommerce_cross_sell_display
-             * @hooked woocommerce_cart_totals - 10
-             */
-            do_action( 'woocommerce_cart_collaterals' );
-            ?>
+        <div class="cart-collaterals card bg-base-200 shadow-xl">
+            <div class="card-body">
+                <?php
+                /**
+                 * Cart collaterals hook.
+                 * @hooked woocommerce_cross_sell_display
+                 * @hooked woocommerce_cart_totals - 10
+                 */
+                do_action( 'woocommerce_cart_collaterals' );
+                ?>
+            </div>
         </div>
         <?php do_action( 'woocommerce_after_cart' ); ?>
     </div>
