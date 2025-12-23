c1de244ec35950b3a1cca1eb12f2b64c8005eeaf
pierre <pierre.bultez@proton.me>
2025-12-23 09:22:40 +0100

    Exclude product descriptions from Relevanssi indexing to improve search relevance.

--- functions.php
+++ functions.php
@@ -361,6 +361,16 @@ add_filter( 'wp_kses_allowed_html', 'trendylux_add_alpine_attributes_to_kses' );
 //}
 //add_filter( 'posts_search', 'trendylux_search_filter', 10, 2 );
 
+/* Force Relevanssi à ignorer le contenu (description) des produits */
+add_filter( 'relevanssi_content_to_index', 'trendylux_relevanssi_no_content', 10, 2 );
+function trendylux_relevanssi_no_content( $content, $post ) {
+    // Si c'est un produit, on vide le texte que Relevanssi s'apprête à lire
+    if ( $post->post_type === 'product' ) {
+        return '';
+    }
+    return $content;
+}
+
 /**
  * Personnalise les arguments des champs du formulaire de paiement de WooCommerce
  * pour y ajouter les classes de DaisyUI et Tailwind. (Version corrigée)
